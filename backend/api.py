6c3a73745b3a9e0ebf9dc5b690ff3334d1b19b47
gooksu3 <65926227+gooksu3@users.noreply.github.com>
2026-05-07 14:53:28 +0900

    modification
    ddd

--- backend/api.py
+++ backend/api.py
@@ -2,18 +2,14 @@ from flask import Flask,jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(
-    app,
-    resources={r"/*": {"origins": "*"}},
-    supports_credentials=True
-)
-
+CORS(app)
 @app.route("/login")
 def test():
     return {
         "message": "success"
     }
-@app.route("/api/1min")
+
+@app.route("/api/initial")
 def home():
     return jsonify("Flask server running")
 
