f99e944c92da53fd98cfdf31fb8cc67bfbe5cda5
gooksu3 <65926227+gooksu3@users.noreply.github.com>
2026-05-07 15:09:58 +0900

    modification
    ....

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 from flask_cors import CORS
 import requests
 
@@ -12,18 +12,19 @@ def test():
 
 @app.route("/api/initial", methods=["GET", "OPTIONS"])
 def initial_api_calling():
+    tm1=request.json['tm1']
+    tm2=request.json['tm2']
     url_wind = 'https://apihub.kma.go.kr/api/typ01/cgi-bin/url/nph-aws2_min?'
     # url_vis = 'https://apihub.kma.go.kr/api/typ01/cgi-bin/url/nph-aws2_min_vis?'
     params = {
     "authKey": "1oWYR_o-SnGFmEf6PlpxQQ",
-    "tm1":"202605021128",
-    "tm2":"202605021130",
+    "tm1":tm1,
+    "tm2":tm2,
     }
     response = requests.get(url_wind,params=params)
     # response = requests.get(url_vis,params=params)
-    row=0
-    list_info=[i.split() for i in response.text.split("\n")[3:-2]]
-    [924,901,898]
+    # row=0
+    # list_info=[i.split() for i in response.text.split("\n")[3:-2]]
     # list_info_ganjulgot=[i for i in list_info if i[1]=="924"] # 간절곶
     # list_info_ulgi=[i for i in list_info if i[1]=="901"] # 울기
     # list_info_jangsaengpo=[i for i in list_info if i[1]=="898"] # 장생포
